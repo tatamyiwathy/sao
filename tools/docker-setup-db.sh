8a02d03f9c88a2ab1784cdf3191b90968cba26f0
tatamyiwathy <jbd00353@gmail.com>
2025-08-05 13:29:12 +0900

    Add superuser check and update Docker setup
    Added is-superuser.py to check for an existing Django superuser. Updated docker-setup-db.sh to use this script and create a superuser if one does not exist. Changed docker-compose.yml to map port 80 to 10000 for the web service.

--- tools/docker-setup-db.sh
+++ tools/docker-setup-db.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # set -eu
-python manage.py makemigrations
 python manage.py migrate
 
 if [ ${SAO_PROFILE} = "prod" ]; then
@@ -12,4 +11,13 @@ fi
 if [ -z "${DJANGO_SUPERUSER_PASSWORD}" ]; then
     echo "❌ DJANGO_SUPERUSER_PASSWORD is not set. Aborting setup."
     exit 1
+fi
+
+python /app/is-superuser.py
+if [ $? -ne 0 ]; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser --noinput
+    echo "Superuser created."
+else
+    echo "Superuser already exists. Skipping creation."
 fi
