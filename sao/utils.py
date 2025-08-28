ca138513a871b037d76dbe2552b587833a4bbbb2
tatamyiwathy <jbd00353@gmail.com>
2025-08-28 18:31:06 +0900

    refactor: Format sesamo form stamp output
    Updated make_sesamo_form_stamp to format the stamp datetime as 'YYYY-MM-DD HH:MM:SS' and use the employee's employee_no for output. This improves consistency and clarity of the generated data.

--- sao/utils.py
+++ sao/utils.py
@@ -145,7 +145,17 @@ def collect_webstamp(employee_no: int, date: datetime.date):
 def make_sesamo_form_stamp(employee: Employee, stamp: datetime.datetime):
     """セサモ形式の打刻データを生成する"""
     # 出退勤管理, 0-03-#-01-#, アクセス制御 , 打刻時刻, 000 区画外, 002 出退勤管理, カード番号, ユーザー, 雇用者番号
-    return ['"出退勤管理"', "", "", '"%s"' % stamp, "", "", "", "", '"%d"' % employee]
+    return [
+        '"出退勤管理"',
+        "",
+        "",
+        f'"{stamp.strftime("%Y-%m-%d %H:%M:%S")}"',
+        "",
+        "",
+        "",
+        "",
+        f'"{employee.employee_no}"'
+    ]
 
 
 def print_strip_sec(total_sec, empty):
